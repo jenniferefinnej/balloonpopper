b456e797793f65e211a326118f19dbcddd6d4716
Michelle Perez <mperez@lsoc.org>
2016-05-16 14:49:33 -0500

    added files

---
+++ controller.rb
@@ -0,0 +1,50 @@
+
+class Player
+	attr_accessor :name, :wins
+
+	def initialize 
+		@player
+		@wins = 0
+	end 
+
+end
+
+class Game
+	def initialize(player)
+		@player = player
+	end
+
+	
+
+	def results
+		puts "How many balloons do you want to pop?"
+		
+
+
+class Balloon_Grid >> Game
+	def balloon_state
+		@balloon_blown = blown
+		elsif when "How many Balloons do you want to pop?"
+			then @balloon_pop = pop 
+	end
+
+end
+
+
+
+end
+
+
+
+
+
+
+#first contruct a player and them going against the computer
+#so initialize so that the player and computer are recognized in the code
+#maybe then make a method so that you structure what the computer can do so like a "results" and see what the comp. can do
+#then go on to making a class for the player to access to the game and allow it to do stuff like the comp.
+#index initialize the game so that the game is allowed to run throughout the index
+#need a time frame - under a minute
+#display a grid of balloons
+#nested data structure
+#the players need to pop the balloons --> they have two states blown up state and pop state
